1e99ed0ba09600ff8bbaba807186745fe66a9520
joshrosso <joshrosso@gmail.com>
2023-11-12 09:49:59 -0700

    add more resilient hypervisor init scripts

--- hypervisor/init/install-nix.sh
+++ hypervisor/init/install-nix.sh
@@ -1,7 +1,6 @@
 ssh root@${NIXADDR} " \
     nixos-generate-config --root /mnt; \
     sed --in-place '/system\.stateVersion = .*/a \
-      nix.package = pkgs.nixUnstable;\n \
       nix.extraOptions = \"experimental-features = nix-command flakes\";\n \
       services.openssh.enable = true;\n \
       services.openssh.passwordAuthentication = true;\n \
@@ -9,5 +8,4 @@ ssh root@${NIXADDR} " \
       users.users.root.initialPassword = \"root\";\n \
     ' /mnt/etc/nixos/configuration.nix; \
     nixos-install --no-root-passwd; \
-    reboot; \
  "
--- hypervisor/init/setup-filesystem.sh
+++ hypervisor/init/setup-filesystem.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
 # inspired by https://github.com/mitchellh/nixos-config/blob/0547ecb427797c3fb4919cef692f1580398b99ec/Makefile#L51-L77
 ssh root@${NIXADDR} " \
-  	parted /dev/sda -- mklabel gpt; \
-		parted /dev/sda -- mkpart primary 512MiB 100\% \
-		parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB; \
-		parted /dev/sda -- set 2 esp on; \
-		sleep 1; \
-		mkfs.ext4 -L nixos /dev/sda1; \
-		mkfs.fat -F 32 -n boot /dev/sda2; \
-		sleep 1; \
-		mount /dev/disk/by-label/nixos /mnt; \
-		mkdir -p /mnt/boot; \
-		mount /dev/disk/by-label/boot /mnt/boot; \
+	umount -R /mnt; \
+	wipefs -a /dev/sda; \
+  	parted -s /dev/sda -- mklabel gpt; \
+	parted /dev/sda -- mkpart primary 512MiB 100\%; \
+	parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB; \
+	parted /dev/sda -- set 2 esp on; \
+	sleep 1; \
+	mkfs.ext4 -L nixos /dev/sda1; \
+	mkfs.fat -F 32 -n boot /dev/sda2; \
+	sleep 1; \
+	mount /dev/disk/by-label/nixos /mnt; \
+	mkdir -p /mnt/boot; \
+	mount /dev/disk/by-label/boot /mnt/boot; \
 "
